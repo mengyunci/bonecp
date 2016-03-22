157d24930bc82c8622112eae7668949ced0b982c
mengyunci <mengyunci@sina.com>
2016-03-22 15:17:36 +0800

    edit properties

---
+++ src/main/java/com/ssgx/ShiroConfiguration.java
@@ -0,0 +1,118 @@
+package com.ssgx;
+
+import com.ssgx.dao.UserRepository;
+import com.ssgx.entity.User;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.credential.DefaultPasswordService;
+import org.apache.shiro.authc.credential.PasswordMatcher;
+import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import javax.servlet.DispatcherType;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by mac on 16/3/21.
+ */
+@Configuration
+public class ShiroConfiguration {
+
+    @Bean
+    public EhCacheManager getCache() {
+        EhCacheManager manager = new EhCacheManager();
+        manager.setCacheManagerConfigFile("classpath:ehcache-shiro.xml");
+        return manager;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filter = new FilterRegistrationBean();
+        filter.setFilter(new DelegatingFilterProxy("shrioFilter"));
+        filter.addInitParameter("targetFilterLifecycle","true");
+        filter.setEnabled(true);
+        filter.addUrlPatterns("/*");
+        filter.setOrder(0);
+        filter.setDispatcherTypes(DispatcherType.ASYNC,DispatcherType.FORWARD,DispatcherType.INCLUDE);
+        return filter;
+    }
+
+
+    /**
+     * 试用Shrio提供的JdbcRealm进行用户身份验证,此处可以配置密码加密策略或者盐等
+     * @param wbm 数据库连接
+     * @param matcher 密码匹配策略
+     * @return
+     */
+    @Bean(name = "jdbcReaml")
+    public JdbcRealm getRealm(@Qualifier("wbm")DataSource wbm,PasswordMatcher matcher) {
+        JdbcRealm realm = new JdbcRealm();
+        realm.setDataSource(wbm);
+        realm.setCredentialsMatcher(matcher);
+        return realm;
+    }
+
+
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getSecurity(JdbcRealm reaml,EhCacheManager ehCacheManager) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setRealm(reaml);
+        manager.setCacheManager(ehCacheManager);
+        return manager;
+    }
+
+    @Bean(name = "shrioFilter")
+    public ShiroFilterFactoryBean getShrioFilterBean(DefaultWebSecurityManager manager) {
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        bean.setSecurityManager(manager);
+        bean.setLoginUrl("/login");
+        bean.setSuccessUrl("/");
+        bean.setUnauthorizedUrl("/login?error");
+
+
+        // FIXME 这些后期可以从配置文件读取
+
+        Map<String,String> chainDefinitionMap = new HashMap<>();
+        chainDefinitionMap.put("/**","authc");
+        chainDefinitionMap.put("/login","anon");
+        chainDefinitionMap.put("/css/**","anon");
+        chainDefinitionMap.put("/js/**","anon");
+        chainDefinitionMap.put("/images/**","anon");
+        chainDefinitionMap.put("/logout","anon");
+
+        bean.setFilterChainDefinitionMap(chainDefinitionMap);
+        return bean;
+
+
+    }
+
+    // 密码加密服务,目前使用Shrio提供的默认方式,后期可以改为配置方式
+    @Bean
+    public PasswordService getPasswordService() {
+        return new DefaultPasswordService();
+    }
+
+    @Bean
+    public PasswordMatcher getPasswordMatcher(PasswordService service) {
+        PasswordMatcher matcher = new PasswordMatcher();
+        matcher.setPasswordService(service);
+        return matcher;
+    }
+
+}
---
+++ src/main/java/com/ssgx/entity/User.java
@@ -0,0 +1,60 @@
+package com.ssgx.entity;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by mac on 16/3/22.
+ */
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Boolean enabled;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotEmpty(message = "用户名不能为空")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    @NotEmpty(message = "密码不能为空")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
